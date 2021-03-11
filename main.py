6103bcd3c4d71cdce0b1588b71c5a7279658fad8
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-03-11 10:29:40 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)    
+
+
+emp1 = Employee('nobu', 'yoshi', 2000)
+emp2 = Employee('test', 'user', 70000)
+
+print(emp1.fullname())
+print(emp2.email)
