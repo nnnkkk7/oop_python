cfcca6302f1a4908c903d585dcaea5c6fbab8f06
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-03-14 14:11:51 +0900

    add raise func

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 class Employee:
+    num_of_emps = 0
+    raise_amount = 1.05
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return '{} {}'.format(self.first, self.last)    
+
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount )
 
 
 emp1 = Employee('nobu', 'yoshi', 2000)
@@ -14,3 +23,13 @@ emp2 = Employee('test', 'user', 70000)
 
 print(emp1.fullname())
 print(emp2.email)
+
+print(emp1.pay)
+emp1.apply_raise()
+print(emp1.pay)
+
+## インスタンスの中身
+print(emp1.__dict__)
+
+## empの数
+print(Employee.num_of_emps)
